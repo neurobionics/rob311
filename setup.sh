38920bc53af2c93177761ec560213e5057dee9cd
Senthur Raj <imsenthur@gmail.com>
2022-09-07 01:26:04 -0400

    Added chown.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 echo "[Setup] Apt Installing required packages..."
 sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential
+echo "[Setup] Changing read/write access..."
+sudo chown $USER -R .
 echo "[Setup] Copying picoload tool..."
 sudo cp ./picoload /bin/
 echo "[Setup] Creating Build directory..."
