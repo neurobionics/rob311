879062dd39f86f9fae497f6c6bec2ad4642f4a9c
Senthur Raj <imsenthur@gmail.com>
2022-09-07 01:14:36 -0400

    Updated setup script.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 echo "[Setup] Apt Installing required packages..."
 sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential
+echo "[Setup] Copying picoload tool..."
+sudo cp ./picoload /bin/
 echo "[Setup] Creating Build directory..."
 mkdir ./mbot-omni-firmware/build
 echo "[Setup] Building..."
 cd ./mbot-omni-firmware/build && cmake ..
 make -j4
+echo "[Setup] Creating /mnt/pico..."
+mkdir /mnt/pico
